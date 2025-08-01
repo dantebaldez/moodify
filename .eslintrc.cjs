610a68b72c93994abf83979920b511659ab5ca02
dantebaldez <dantegl2000@gmail.com>
2025-08-01 11:48:22 -0300

    feat: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+  },
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Moodify 🎧
+
+O **Moodify** é um app de playlists animadas onde o usuário escolhe seu humor (animado, relax ou foco) e recebe sugestões visuais de playlists compatíveis. O app combina uma experiência visual fluida com microinterações e autenticação via Firebase.
+
+## 🛠️ Tech Stack
+
+- React 18 + Vite
+- TypeScript
+- styled-components + ThemeProvider
+- Framer Motion
+- Firebase Auth
+- React Router v6
